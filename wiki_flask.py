f1342a8316ba9a3cbe593b2ca6c66f020bb5e578
Devyani Kota <divs.passion.18@gmail.com>
2015-09-10 20:20:15 +0530

    Added the flask app file

---
+++ wiki_flask.py
@@ -0,0 +1,41 @@
+from flask import Flask
+from flask import request
+from bs4 import BeautifulSoup
+import requests
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def pop_wiki_link():
+    # link = raw_input("Enter the wikipedia link : ")
+    link = request.args.get('link')
+    response = requests.get(link)
+    for link in BeautifulSoup(response.text, 'html.parser').find_all('a'):
+        if link.has_attr('href'):
+            sublink = link['href']
+            print sublink
+            if (sublink.startswith('/wiki/Wikipedia')):
+                pass
+            elif (sublink.startswith('/wiki')):
+                new_link = (
+                    "https://en.wikipedia.org/wiki/Special:WhatLinksHere/"
+                    + sublink.split('/')[2]
+                     )
+                resp = requests.get(new_link)
+                maxcount, count = 0, 0
+                for slink in BeautifulSoup(
+                  resp.text, 'html.parser').find_all('a'):
+                    if slink.has_attr('href'):
+                        count += 1
+                    if count > maxcount:
+                        l = slink
+                        maxcount = count
+                    else:
+                        continue
+    return l
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
